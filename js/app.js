329bbf2447db13272cc76aea2487d9d4c286f98b
Daniel Bielecki <bd1988@interia.pl>
2021-03-03 17:59:40 +0100

    tetrominos drawn

--- js/app.js
+++ js/app.js
@@ -1,3 +1,64 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    console.log('Hello!')
+    
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(grid.querySelectorAll('div'));
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#start-button')
+    const width = 10;
+    const height = 20;
+    let currentPosition = 4;
+
+    console.log(scoreDisplay);
+    console.log(startBtn);
+    console.log(grid);
+    console.log(squares);
+
+
+    const lTetromino = [
+        [1, width+1, width*2+1, width*2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ];
+
+    const zTetromino = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ];
+
+    const tTetromino  = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ];
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ];
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+//Randomly select Tetromino
+
+let random = Math.floor(math.random()*theTetrominoes.length)
+let currentRotation = 0;
+let current = theTetrominoes[random][currentRotation];
+
+// move the Tetromino down
+
+
+
 })
