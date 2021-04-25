2b1e9d9112c734622b086ef6ebcf9f184df711c1
Daniel Bielecki <bd1988@interia.pl>
2021-04-25 15:30:22 +0200

    added next tetromino display

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const scoreDisplay = document.querySelector('#score');
     const startBtn = document.querySelector('#start-button')
     const width = 10;
+    
+    
 
     const lTetromino = [
         [1, 2, width+1, width*2+1],
@@ -42,11 +44,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
     ]
 
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
-
+   
     let currentPosition = 4;
     let currentRotation = 0;
     let random = Math.floor(Math.random()*theTetrominoes.length);
-    console.log(random)
+    
     let current = theTetrominoes[random][currentRotation];
     
     //Draw the Tetromino
@@ -66,6 +68,21 @@ document.addEventListener('DOMContentLoaded', ()=> {
     //make the tetromino move down every second
 
     timerId = setInterval(moveDown, 1000);
+   
+    //assign function to keyCodes
+
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft()
+        } else if (e.keyCode === 38) {
+            rotate()
+        } else if (e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+    document.addEventListener('keyup', control)
 
     //move down function
 
@@ -103,4 +120,57 @@ document.addEventListener('DOMContentLoaded', ()=> {
         
         draw();
     }
+
+    //move the tetromino right, unless is at the edge or there is a blockage
+
+    function moveRight() {
+        undraw();
+        const isAtRightEdge = current.some( index => (currentPosition + index) % width === width -1);
+
+        if(!isAtRightEdge) currentPosition += 1;
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -=1;
+            }
+
+            draw();
+    }
+
+    //rotete the termonio
+
+    function rotate() {
+        undraw();
+        currentRotation++;
+        if(currentRotation === current.length) {
+            currentRotation = 0;
+        }
+        current = theTetrominoes[random][currentRotation];
+        draw()
+    }
+
+    //show up next teromino
+
+    const displaySquares = document.querySelectorAll('mini-grid div');
+    const displayWidth = 4;
+    let displayIndex = 0;
+
+    //the Tetrominos without rotations
+
+    const upNextTetrominoes = [
+        [1, displayWidth+1, displayWidth*2+1,2],    //lTetromino
+        [0, displayWidth, displayWidth+1, displayWidth*2+1],    //zTetromino
+        [1, displayWidth, displayWidth+1, displayWidth+2],     //tTermonio
+        [0, 1, displayWidth, displayWidth+1]    //oTetromino
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1]     //iTetromino
+    ]
+
+    //display the Shape in the mini-grid display
+
+    function displayShape() {
+        //remove any trace of a tetromino class from the entire grid
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino')
+        })
+    }
+
 })
